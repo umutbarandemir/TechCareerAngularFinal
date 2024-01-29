53a371c55c600cf7d8a553c1a03bc426d6f7e464
umutbarandemir <baran292907@hotmail.com>
2024-01-29 21:09:22 +0300

    homepage component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,FooterComponent,MainComponent],
+  imports: [RouterOutlet,NavbarComponent,FooterComponent,MainComponent,HomepageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

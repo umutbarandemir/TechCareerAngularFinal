48ca66fe3d82e6b1927ec27486aa8f770ce51508
umutbarandemir <baran292907@hotmail.com>
2024-01-30 17:32:59 +0300

    icons, sub_title

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -28,36 +28,37 @@ export class MainComponent implements OnInit{
     }
   ];
 
+ 
   projectsCard:ProjectsCard[]=[
     { 
-      id:1,icon:"flaticon-seo",title:"OnePage Portfolio",description:"Onepage portfolio website that i built for the final project for Techcareer Angular Frontend Bootcamp using angular knowledge i gained during the 5 weeks long training.",githublink:"https://github.com/umutbarandemir/OnePagePortfolio",
+      id:1,icon:"fa-solid fa-file",title:"OnePage Portfolio",description:"Onepage portfolio website that i built for the final project for Techcareer Angular Frontend Bootcamp using angular knowledge i gained during the 5 weeks long training.",githublink:"https://github.com/umutbarandemir/OnePagePortfolio",
     },
     { 
-      id:2,icon:"flaticon-process",title:"Forum Website with React",description:"Created a forum website to test my react and nodejs knowledge where users were able to login, register, create threads, search for threads, leave comments on threads and like threads. Used mongoDB for the first time for cloud storage.",githublink:"https://github.com/umutbarandemir/ForumWebsite",
+      id:2,icon:"fa-solid fa-folder-open",title:"Forum Website with React",description:"Created a forum website to test my react and nodejs knowledge where users were able to login, register, create threads, search for threads, leave comments on threads and like threads. Used mongoDB for the first time for cloud storage.",githublink:"https://github.com/umutbarandemir/ForumWebsite",
     },
     { 
-      id:3,icon:"flaticon-discuss-issue",title:"GameApp in Android Studio",description:"To sum up all my knowledge about android studio java, I created a game app. Started as a stand alone game then turned into a game app with multiple games, main page, difficulty scenes and game scenes.",githublink:"https://github.com/umutbarandemir/GameAppAndroid",
+      id:3,icon:"fa-solid fa-code",title:"GameApp in Android Studio",description:"To sum up all my knowledge about android studio java, I created a game app. Started as a stand alone game then turned into a game app with multiple games, main page, difficulty scenes and game scenes.",githublink:"https://github.com/umutbarandemir/GameAppAndroid",
     },
     { 
-      id:4,icon:"flaticon-development",title:"IMDB type Critic Website with React",description:"Built a full-stack web app to allow users to search for movies, video games or tv show titles and leave reviews using React, NodeJS and MongoDB.",githublink:"https://github.com/umutbarandemir/MyCritic",
+      id:4,icon:"fa-solid fa-file-code",title:"IMDB type Critic Website with React",description:"Built a full-stack web app to allow users to search for movies, video games or tv show titles and leave reviews using React, NodeJS and MongoDB.",githublink:"https://github.com/umutbarandemir/MyCritic",
     },
     { 
-      id:5,icon:"flaticon-idea",title:"TetrisBot in Python",description:"Utilized python basics and pygame library to create a python game then implemented a bot to play it without using advanced python libraries.",githublink:"https://github.com/umutbarandemir/tetris_bot",
+      id:5,icon:"fa-solid fa-gear",title:"TetrisBot in Python",description:"Utilized python basics and pygame library to create a python game then implemented a bot to play it without using advanced python libraries.",githublink:"https://github.com/umutbarandemir/tetris_bot",
     },
     { 
-      id:6,icon:"flaticon-idea-1",title:"MovieRecomendation System with ML",description:"A movie recommendation system that uses K-NN algorithm to find similarities between user activities and suggests movies to users based on similar users.",githublink:"https://github.com/umutbarandemir/MovieRec",
+      id:6,icon:"fa-solid fa-star",title:"MovieRecomendation System with ML",description:"A movie recommendation system that uses K-NN algorithm to find similarities between user activities and suggests movies to users based on similar users.",githublink:"https://github.com/umutbarandemir/MovieRec",
     },
   ];
-
+  
   educationCard:EducationCard[] = [
     {
-      id:1,icon:"flaticon-seo",title:"High School",location:"Tülay Basaran Anadolu Lisesi, SAMSUN / ILKADIM , ",date:"2015-2019",description:"Root of the love and the passion i have for the software world and the constant hunger to learn resides on the growth i had during these years.",description2:"",buttonId:"highSchoolBtn",pdfPath: "../../pdfs/lisebelgesi.pdf",
+      id:1,icon:"fa-solid fa-school",title:"High School",location:"Tülay Basaran Anadolu Lisesi, SAMSUN / ILKADIM , ",date:"2015-2019",description:"Root of the love and the passion i have for the software world and the constant hunger to learn resides on the growth i had during these years.",description2:"",buttonId:"highSchoolBtn",pdfPath: "../../pdfs/lisebelgesi.pdf",
     },{
-      id:2,icon:"flaticon-development",title:"College",location:"Istanbul Aydin University, KUCUKCEKMECE / ISTANBUL , ",date:"2019-2023",description:"Building a solid foundation that includes software basics, algorithms and building projects in variety of languages. Tapping into the limitless potential of software world.",description2:"",buttonId:"collegeBtn",pdfPath: "../../pdfs/ünibelge.pdf",
+      id:2,icon:"fa-solid fa-graduation-cap",title:"College",location:"Istanbul Aydin University, KUCUKCEKMECE / ISTANBUL , ",date:"2019-2023",description:"Building a solid foundation that includes software basics, algorithms and building projects in variety of languages. Tapping into the limitless potential of software world.",description2:"",buttonId:"collegeBtn",pdfPath: "../../pdfs/ünibelge.pdf",
     },{
-      id:3,icon:"flaticon-process",title:"Internships",location:"Remote , ",date:"SUMMER 2023",description:"Kreatif Fabrika, August 2024",description2:"To finish my college journey i did 28 days of intership in this firm. I learned and used PHP Laravel along with the html,css and javascript.",buttonId:"internshipBtn",pdfPath: "../../pdfs/tubitak.pdf",
+      id:3,icon:"fa-solid fa-user-tie",title:"Internships",location:"Remote , ",date:"SUMMER 2023",description:"Kreatif Fabrika, August 2024",description2:"To finish my college journey i did 28 days of intership in this firm. I learned and used PHP Laravel along with the html,css and javascript.",buttonId:"internshipBtn",pdfPath: "../../pdfs/tubitak.pdf",
     },{
-      id:4,icon:"flaticon-process",title:"After College",location:"Remote , ",date:"FALL 2023",description:"After completing my internships and graduation i got an offer from TUBITAK BILGEM YTE BOOTCAMP 2023 to attend their online bootcamp that had subjects such as Java, Figma, Springboot. Completing these courses and doing some freelance jobs brought me to end of 2023.",description2:"To start the 2024, I have been taking an Angular Frontend Bootcamp via TechCareer.",buttonId:"afterCollegeBtn",pdfPath: "../../pdfs/tubitak.pdf",
+      id:4,icon:"fa-solid fa-laptop-code",title:"After College",location:"Remote , ",date:"FALL 2023",description:"After completing my internships and graduation i got an offer from TUBITAK BILGEM YTE BOOTCAMP 2023 to attend their online bootcamp that had subjects such as Java, Figma, Springboot. Completing these courses and doing some freelance jobs brought me to end of 2023.",description2:"To start the 2024, I have been taking an Angular Frontend Bootcamp via TechCareer.",buttonId:"afterCollegeBtn",pdfPath: "../../pdfs/tubitak.pdf",
     },
   ];
   viewPdf(pdfPath: string) {
